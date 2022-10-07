cfbb4c9cf354b9cd812203d50502de42f2bdbfe8
BlazeRavager1 <89845937+BlazeRavager1@users.noreply.github.com>
2022-10-07 17:33:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-
 var recognition = new SpeechRecognition();
 
 function start() {
-  recognition.start;
+  recognition.start();
 }
 
 recognition.onresult = function (event) {
   console.log(event);
 
-  var Content = event.results[0][0].transcript;
+  var Content = event.results[0][0].transcript.toLowerCase();
   console.log(Content);
   if (Content == "selfie") {
     speak();
   }
-};
+}
 
 Webcam.set({
   width: 360,
   height: 250,
-  image_format: "png",
+  image_format: 'png',
   png_quality: 90,
 });
 
@@ -40,7 +39,7 @@ setTimeout(function () {
     speak_data = "Taking your Selfie in 5 Seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
-}, 5000);
+}, 5000)
     
     setTimeout(function () {
       img_id = "selfie2";
@@ -51,21 +50,22 @@ setTimeout(function () {
     }, 10000);
 
     setTimeout(function () {
-      img_id = "selfie_3";
+      img_id = "selfie3";
       take_snapshot();
     }, 15000);
 }
 
 function take_snapshot() {
+  console.log(img_id);
     Webcam.snap(function (data_uri) {
         if (img_id == "selfie1") {
-          document.getElementById("result").innerHTML ='<img id = "selfie1" src = "' + data_uri + '">';
+          document.getElementById("result1").innerHTML ='<img id = "selfie1" src = "' + data_uri + '">';
         }
         if (img_id == "selfie2") {
-          document.getElementById("result").innerHTML ='<img id = "selfie2" src = "' + data_uri + '">';
+          document.getElementById("result2").innerHTML ='<img id = "selfie2" src = "' + data_uri + '">';
         }
         if (img_id == "selfie3") {
-          document.getElementById("result").innerHTML ='<img id = "selfie3" src = "' + data_uri + '">';
+          document.getElementById("result3").innerHTML ='<img id = "selfie3" src = "' + data_uri + '">';
         }
     
   });
